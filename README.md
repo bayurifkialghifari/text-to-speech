b7e1dc38a76e379350896ca24450c887d06da2d2
bayurifkialghifari <bayurifkialgh@gmail.com>
2025-05-09 15:52:58 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# tts
+## Text To Speech
+
+Text to speech
+
+## Instalation
+
+1. git clone
+2. yarn
+3. cp .env.example .env
+4. yarn start
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const gTTS = require("gtts");
+const moment = require("moment");
+const app = express();
+
+// Dot env
+require("dotenv").config();
+
+// Static files
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/tts", (req, res) => {
+  console.log(req.body);
+  const text = req.body.text;
+  const lang = req.body.lang || "en";
+  const gtts = new gTTS(text, lang);
+  const filename = `public/${moment().format("YYYYMMDD_HHmmss")}.wav`;
+  gtts.save(filename, function (err, result) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send({
+      message: "Text to speech conversion successful",
+      filename: filename,
+    });
+  });
+});
+
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `App listening at https://${process.env.HOST}:${process.env.PORT}`
+  );
+});
